cf30dbb22216eececb42479c2180bb39af3945d4
Jesse Garcilazo <jjgarcilazo@gmail.com>
2020-05-07 20:35:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves ["Doc", "Dopey", "Bashful", "Grumpy"]      # code an argument here
-puts roll_call_dwarves  # Your code here
+puts "roll_call_dwarves"  # Your code here
 end
 
 def summon_captain_planet# code an argument here
