8dacc7d314e447423efb36584b4b15a84d05d10c
Jesse Garcilazo <jjgarcilazo@gmail.com>
2020-05-07 20:48:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
-def roll_call_dwarves(dwarves)  
+def roll_call_dwarves(dwarves)
   dwarves.each_with_index do |name, index|
-    index += 1 
+    index += 1
     puts "#{index}. #{name}"
  end
 end
