497d00cd0dc3dd922643417af0be0f01a287cace
Jesse Garcilazo <jjgarcilazo@gmail.com>
2020-05-07 20:48:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
-def roll_call_dwarves("Doc", "Dopey", "Bashful", "Grumpy")     # code an argument here
-puts "roll_call_dwarves"  # Your code here
+def roll_call_dwarves(dwarves)  
+  dwarves.each_with_index do |name, index|
+    index += 1 
+    puts "#{index}. #{name}"
+ end
 end
 
 def summon_captain_planet# code an argument here
